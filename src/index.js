89064bca80b64c6a6542263d6aa6ae773527ebe3
Serhii Khomenchuk <serhiikhomenchuk@gmail.com>
2023-05-07 00:51:55 +0300

    problems here

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
-// import SimpleLightbox from "simplelightbox";
-// // Додатковий імпорт стилів
+
+// import SimpleLightbox from "simplelightbox"
 // import "simplelightbox/dist/simple-lightbox.min.css";
-import simpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from "notiflix";
+import axios from "axios";
+
 const form = document.querySelector("#search-form");
 const gallery = document.querySelector(".gallery");
 let searchQuery = form.searchQuery.value
 const guard = document.querySelector('.js-guard');
 // console.log(gallery);
-const page = 1;
+let page = 1;
 const per_page = 40;
+// var litebox = new SimpleLightbox('.some-element a');
+
+const options = {
+  root: null,
+  rootMargin: '500px',
+  threshold: 0.0,
+};
+const observer = new IntersectionObserver(onInfinityScroll, options);
+
 form.addEventListener("submit", onSubmit);
-const litebox = new SimpleLightbox('.some-element a');
 function onSubmit(evt) {
   evt.preventDefault();
   searchQuery = evt.target.elements.searchQuery.value.trim();
   // console.log(searchQuery);
     fetchImages()
-      .then((data) =>
-        {if (!data.length) {
+      .then((data) => {
+        if (!data.hits.length) {
         Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+      gallery.innerHTML =""
+        return  
         }
+        if (data.hits) {
+        gallery.innerHTML = '';
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        // simpleLightbox.refresh();
+        }
+        if (page !== data.totalHits) {
+        console.log(page);
+        console.log(data.totalHits);
+        observer.observe(guard);
+      }
         gallery.innerHTML = createMarkup(data.hits);
 
       })
@@ -30,7 +51,10 @@ function onSubmit(evt) {
   
 }
 
-function fetchImages() {
+
+
+
+async function fetchImages() {
 
     const BASE_URL = "https://pixabay.com/api/"
     const searchParams = new URLSearchParams({
@@ -43,20 +67,44 @@ function fetchImages() {
         per_page:`${per_page}`
     })
   const URL = `${BASE_URL}?${searchParams}`;
-   return fetch(URL).then((resp) => { 
-        if (!resp.ok || resp.statusCode === 404) {
-           throw new Error(resp.statusText)
-        }
-        
-        return resp.json()
-   }) 
+    try {
+    const resp = await axios.get(URL);
+    const { data } = resp;
+    console.log(resp);
+  return data;
+  } catch (error) {
+    console.error(error);
+  }
+   
 }
 
- 
+ function onInfinityScroll(entries, observer) {
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+      page += 1;
+      console.log(entry);
+      fetchImages(page).then(data => {
+        gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+        // simpleLightbox.refresh();
+
+        const totalPages = Math.ceil(data.totalHits / per_page);
+        if (page === totalPages) {
+          console.log("the end");
+          observer.unobserve(guard);
+          Notiflix.Notify.info(
+            'We are sorry, but you have reached the end of search results.'
+          );
+        }
+      }).catch(err => console.log(err));
+    }
+  });
+ }
+
 function createMarkup(arrOfImgs) {
   return arrOfImgs.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
     `<div class="photo-card">
-        <a  href="${webformatURL}"><img class = "image" src="${largeImageURL}" alt="${tags}" loading="lazy" />
+        <a  href="${webformatURL} class="gallery-link"><img class = "image" src="${largeImageURL}" alt="${tags}" loading="lazy" />
         </a>
           <div class="info">
           <p class="info-item">
@@ -81,3 +129,11 @@ function createMarkup(arrOfImgs) {
   
     
 }
+let litebox = new SimpleLightbox(".gallery .gallery-link", {
+      // enableKeyboard,
+      captionDelay: 250,
+      captionsData: "alt",
+      
+
+   
+})
