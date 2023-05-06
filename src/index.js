dc20603dca62c65a87481ca3aa0fcf7822e17c65
Serhii Khomenchuk <serhiikhomenchuk@gmail.com>
2023-05-06 23:15:29 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 // import SimpleLightbox from "simplelightbox";
 // // Додатковий імпорт стилів
 // import "simplelightbox/dist/simple-lightbox.min.css";
-import simpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+// import simpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
 const form = document.querySelector("#search-form");
 const gallery = document.querySelector(".gallery");
-const searchQuery = form.searchQuery.value
+let searchQuery = form.searchQuery.value
+const guard = document.querySelector('.js-guard');
 // console.log(gallery);
-
+const page = 1;
+const per_page = 40;
 form.addEventListener("submit", onSubmit);
-
+const litebox = new SimpleLightbox('.some-element a');
 function onSubmit(evt) {
   evt.preventDefault();
+  searchQuery = evt.target.elements.searchQuery.value.trim();
+  // console.log(searchQuery);
     fetchImages()
-      .then((data) => gallery.innerHTML = createMarkup(data.hits))
-        .catch((err) => console.log(err))
+      .then((data) =>
+        {if (!data.length) {
+        Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+        }
+        gallery.innerHTML = createMarkup(data.hits);
+
+      })
+      .catch((err) => console.log(err))
   
   
 }
 
 function fetchImages() {
+
     const BASE_URL = "https://pixabay.com/api/"
     const searchParams = new URLSearchParams({
         key: "36056334-58c346fb23a68bf9b46b38280",
-        q:"cat",
+        q:`${searchQuery}`,
         image_type: "photo",
         orientation: "horizontal",
-        safesearch: true
+      safesearch: true,
+      page: `${page}`,
+        per_page:`${per_page}`
     })
   const URL = `${BASE_URL}?${searchParams}`;
    return fetch(URL).then((resp) => { 
@@ -39,11 +52,11 @@ function fetchImages() {
    }) 
 }
 
-  
+ 
 function createMarkup(arrOfImgs) {
   return arrOfImgs.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-    `<div class="gallery">
-        <a href="${webformatURL}"><img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+    `<div class="photo-card">
+        <a  href="${webformatURL}"><img class = "image" src="${largeImageURL}" alt="${tags}" loading="lazy" />
         </a>
           <div class="info">
           <p class="info-item">
@@ -68,4 +81,3 @@ function createMarkup(arrOfImgs) {
   
     
 }
-let simplelightbox = new simpleLightbox('.gallery a');
